057e264c0d158ee69af6432e96d5a02262f91173
alex <alex.inntekt@gmail.com>
2020-03-15 12:56:43 +0200

    renamed project

--- src/wad/settings.py
+++ src/wad/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'src.urls'
+ROOT_URLCONF = 'wad.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'src.wsgi.application'
+WSGI_APPLICATION = 'wad.wsgi.application'
 
 
 # Database
