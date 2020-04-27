0fee591584da6a5287e2411acd13ef9f74f56e80
alexinntekt <alex.inntekt@gmail.com>
2020-04-27 23:05:08 +0300

    sp

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SearchView
+from .views import SearchView, ListView
 
 urlpatterns = [
 	path('search', SearchView.as_view(), name='search'),
+	path('items', ListView.as_view(), name='listview'),
 ]
--- src/shop/views.py
+++ src/shop/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import generic
+from .models import Item
+
+class ListView(generic.ListView):
+    template_name = 'list.html'
+
+    def get_queryset(self):
+        return Item.objects.all()
 
 class SearchView(TemplateView):
-	template_name = 'search.html'
+    template_name = 'search.html'
+
+    
