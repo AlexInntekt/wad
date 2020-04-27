0ceeeaa536c3a51aa5e2fdc6267ff90de87636ae
alexinntekt <alex.inntekt@gmail.com>
2020-04-27 22:32:30 +0300

    working on Item database table

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+	name = models.CharField(max_length=100)
+	# brand = models.CharField()
+	car_brand = models.CharField(max_length=100)
+	car_model = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=8, decimal_places=2)
+	stock = models.IntegerField()
+
+	def __str__(self):
+		return "#{} {} ".format(self.id, self.name)
