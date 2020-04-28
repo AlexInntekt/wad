b7fcdca24820889d8722b66c8db57f259990afc3
alexinntekt <alex.inntekt@gmail.com>
2020-04-28 09:29:02 +0300

    sp

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import generic
+from django.views.generic.list import ListView
 from .models import Item
 
-class ListView(generic.ListView):
+class ListView(ListView):
     template_name = 'list.html'
+    model = Item
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
     def get_queryset(self):
         return Item.objects.all()
