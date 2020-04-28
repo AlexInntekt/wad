406e7c0ae904ec21fc7e9ab5ce1ab7c1d1fb931f
alexinntekt <alex.inntekt@gmail.com>
2020-04-28 09:42:12 +0300

    sp

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import generic
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
+
 from .models import Item
+
+
 
 class ListView(ListView):
     template_name = 'list.html'
@@ -18,4 +22,8 @@ class ListView(ListView):
 class SearchView(TemplateView):
     template_name = 'search.html'
 
-    
+    def get(self, request):
+        return render(request, 'search.html')
+
+    def post(self, request):
+        return redirect('listview')
