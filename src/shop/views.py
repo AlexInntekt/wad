f315d4daa89d8989c5dbf695b946990743564f0c
alexinntekt <alex.inntekt@gmail.com>
2020-04-28 09:52:15 +0300

    sp

--- src/shop/views.py
+++ src/shop/views.py
@@ -16,14 +16,19 @@ class ListView(ListView):
         context = super().get_context_data(**kwargs)
         return context
 
-    def get_queryset(self):
+    def get_queryset(self, **kwargs):
+        print(kwargs)
+        print(self.request)
         return Item.objects.all()
 
 class SearchView(TemplateView):
     template_name = 'search.html'
 
-    def get(self, request):
+    def get(self, request, **kwargs):
         return render(request, 'search.html')
 
-    def post(self, request):
+    def post(self, request, **kwargs):
+        filters = {}
+        print(kwargs)
+        print(request.POST)
         return redirect('listview')
