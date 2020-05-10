71200c03566ab163493646e49c5bd083a7b922da
alexinntekt <alex.inntekt@gmail.com>
2020-05-11 00:31:48 +0300

    sp

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-
+from django.contrib.postgres.fields import JSONField
 
 
 # Create your models here.
 class Item(models.Model):
-	name = models.CharField(max_length=100)
-	# brand = models.CharField()
-	car_brand = models.CharField(max_length=100)
-	car_model = models.CharField(max_length=100)
-	price = models.DecimalField(max_digits=8, decimal_places=2)
-	stock = models.IntegerField()
+    name = models.CharField(max_length=100)
+    data = JSONField(null=True)
+    # car_model = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField()
 
-	def __str__(self):
-		return "#{} {} ".format(self.id, self.name)
+    def __str__(self):
+        return "#{} {} ".format(self.id, self.name)
 
 
 class Image(models.Model):
