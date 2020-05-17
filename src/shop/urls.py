63914e3670fd68786b07ba15d5c55e9f72f601d8
alexinntekt <alex.inntekt@gmail.com>
2020-05-17 23:59:52 +0300

    sp

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -4,6 +4,7 @@ from .views import SearchView, ListView, DetailView, AdminView, AddItemAdminView
 urlpatterns = [
 	path('search', SearchView.as_view(), name='search'),
 	path('items', ListView.as_view(), name='listview'),
+    path('items/by_category=<str:category>', ListView.as_view(), name='listview_category'),
     # url(r'detail/(?P<pk>\d+)', DetailView.as_view(), name='detailview'),
     path('detail/id=<int:id>', DetailView.as_view(), name='detailview'),
     path('administrare', AdminView.as_view(), name='adminview'),
